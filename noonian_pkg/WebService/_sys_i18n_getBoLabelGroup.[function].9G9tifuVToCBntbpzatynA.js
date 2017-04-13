875f79728e51d5b96f0b8d077366e02907def075
Gene <gene@noonian.org>
2017-04-13 16:26:17 -0700

    Fail nicely when getBoLabelGroup isn't provided a class name

--- noonian_pkg/WebService/_sys_i18n_getBoLabelGroup.[function].9G9tifuVToCBntbpzatynA.js
+++ noonian_pkg/WebService/_sys_i18n_getBoLabelGroup.[function].9G9tifuVToCBntbpzatynA.js
@@ -1,5 +1,9 @@
 function (req, queryParams, I18n) {
     
+    if(!queryParams.className) {
+        return {result:{}};
+    }
+    
     return I18n.getBoLabelGroup(queryParams.className, req.user).then(function(fieldLabels) {
         return {result:fieldLabels};
     });
