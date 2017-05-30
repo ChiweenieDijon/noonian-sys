ff381f939e141e000181dea35c7a072027cec13c
Gene <gene@noonian.org>
2017-05-30 16:27:01 -0700

    Better formatting/menus on tinymce editor

--- noonian_pkg/FieldTypeUiSpec/dbui.addon.tinymce_editor.[controller_fn].yzItsBrUQr-SZfH53sXVVQ.js
+++ noonian_pkg/FieldTypeUiSpec/dbui.addon.tinymce_editor.[controller_fn].yzItsBrUQr-SZfH53sXVVQ.js
@@ -1,6 +1,8 @@
 function ($scope) {
     $scope.tinymceOptions = {
-        plugins: 'link code',
-        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+        plugins: 'link code table textcolor colorpicker',
+        toolbar: 'undo redo | styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | table | code',
+        menubar:'edit insert view format table',
+        height:'400'
       };
 }
